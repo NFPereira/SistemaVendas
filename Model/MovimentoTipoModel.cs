2f2a2735e176dbc5eee3b967a52d370ae943dc3f
NFPereira <nilsonfpereira@hotmail.com>
2023-12-23 21:39:28 -0300

    Testando

--- Model/MovimentoTipoModel.cs
+++ Model/MovimentoTipoModel.cs
@@ -3,8 +3,8 @@
     public class MovimentoTipoModel
     {
         public int Id { get; set; }
-        public string Codigo { get; set; }
-        public string Nome { get; set; }
+        public string? Codigo { get; set; }
+        public string? Nome { get; set; }
         public bool Ativo { get; set; }
     }
 }
